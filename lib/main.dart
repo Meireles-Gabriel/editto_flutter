577d2a61559d8ef5b7ffd60982127a9fed05f4ac
Gabriel Meireles <contato.gabrielmeireles@gmail.com>
2025-03-13 17:30:57 -0300

    Add Firebase integration and configure plugins for cloud services

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'utilities/firebase_options.dart'; // Arquivo gerado pelo Firebase CLI
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -12,7 +18,13 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Hello World!',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+            ),
+          ),
         ),
       ),
     );
